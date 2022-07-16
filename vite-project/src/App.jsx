7e36ef0948aca14048b0468012d0d25115a4e383
SameerDalal <sameerdalal747@gmail.com>
2022-07-15 23:46:16 -0400

    updated the src folder with prev code

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import TempusProfile from './components/tempusProfile/TempusProfile';
+import TempusSearch from './components/tempusSearch/TempusSearch';
+import Navbar from './components/tempusHome/navbar/Navbar';
+import Footer from './components/tempusHome/footer/Footer';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import './App.css';
+import React from 'react';
+import TempusHome from './components/tempusHome/TempusHome';
 
+const App = () => {
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <TempusHome />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+      <App />
+  </StrictMode>
+);
+reportWebVitals(); 
