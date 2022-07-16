9cb4911a546eb6de12a8a998e8d5252e09baca07
SameerDalal <sameerdalal747@gmail.com>
2022-07-16 19:39:57 -0400

    added to searchbar

--- vite-project/src/components/tempusSearch/searchBar/SearchBar.jsx
+++ vite-project/src/components/tempusSearch/searchBar/SearchBar.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import './searchBar.css'
+import React from 'react';
+import './searchBar.css';
 
 
 const SearchBar = () => {
@@ -7,6 +7,8 @@ const SearchBar = () => {
       <div className="search__bar-container">  
         <div className="search__bar">
             <input type="text" placeholder="Search" />
+            <div class="fa fa-search"></div>
+            <div class="fa fa-times"></div>
         </div>
       </div>
     )
