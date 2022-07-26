8f9101e42839e2286642ede8f40119634234b460
SameerDalal <sameerdalal747@gmail.com>
2022-07-25 21:17:30 -0400

    finished the navbar for the desktop version:

--- vite-project/src/components/tempusHome/navbar/Navbar.jsx
+++ vite-project/src/components/tempusHome/navbar/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import './navbar.css'
 import arrowLeft from '../../../imgs/ArrowLeft.png';
+import profileImg from '../../../imgs/profile_logo.png';
 
 const Navbar = () => {
   return (
     <div className = "my__navbar">
       <div className="my__navbar-title" >
-        <a href=""> <img src={arrowLeft} alt="arrowLeft" /> </a>
+        <a id="arrowLeft" href=""> <img src={arrowLeft} alt="arrowLeft" /> </a>
         <h3>Tempus</h3>
+        <input id="searchInput" type="text" placeholder="Search" />
+        <a id="profileImg" href="http://127.0.0.1:5173/profile"> <img src={profileImg} alt="profileImg" width="80px" /> </a>
       </div>
     </div>
   )
